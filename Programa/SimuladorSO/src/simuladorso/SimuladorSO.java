f0072eddd02ef9217dc70911f5fa6724109091c6
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2026-09-06 00:06:47 -0600

    Primeras Interfaz
    se bosquejan las primeras interfaces para acomodar ideas y planficar la arquiterctura del programa

---
+++ Programa/SimuladorSO/src/simuladorso/SimuladorSO.java
@@ -0,0 +1,18 @@
+
+package simuladorso;
+
+/**
+ *
+ * @author elder
+ */
+
+
+public class SimuladorSO {
+
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+}
+
