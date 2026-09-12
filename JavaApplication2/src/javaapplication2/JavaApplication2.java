0c9c65e812eb5ce5b6828a4370477fbc71068322
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2026-09-11 23:13:46 -0600

    Launch Swing interface from main

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -3,14 +3,6 @@ package javaapplication2;
 public class JavaApplication2 {
 
     public static void main(String[] args) {
-
-        SimuladorSO simulador =
-                new SimuladorSO();
-
-        simulador.encender();
-
-        System.out.println(
-                "Sistema listo para iniciar interfaz."
-        );
+        VentanaPrincipal.mostrar();
     }
 }
